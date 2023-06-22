8255247377736bcb2857fe6182b015c90fc43a18
armuh16 <muhammadarif.aq@icloud.com>
2023-06-23 02:18:31 +0800

    error handling better

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+func main() {
+	if err := userJustShitTheirPants(); err != nil {
+		changeDiapers()
+	}
+	if err := Bar(); err != nil {
+		// handler
+	}
+}
+
+func Foo() error {
+	return nil
+}
+
+func Bar() error {
+	if err := Foo(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func changeDiapers() (int, error) {
+	return 10, nil
+}
+
+func userJustFarted() (int, error) {
+	return 10, nil
+}
+
+func userJustShitTheirPants() error {
+	return nil
+}
